f814365a0042acc027373d307a7a14b6db25a098
Denis Nikolaev <150382827+NeX0Zz@users.noreply.github.com>
2025-03-25 11:54:23 +0300

    Initial Commit

---
+++ KreaAI/KreaAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KreaAIApp.swift
+//  KreaAI
+//
+//  Created by Денис Николаев on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct KreaAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
